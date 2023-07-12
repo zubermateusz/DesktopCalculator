bf9f0228e51070c973d8801b7ed2126ddf8c3280
Mateusz <zubermateusz@gmail.com>
2023-07-12 18:59:22 +0200

    add task Implement Runnable

---
+++ Topics/Anonymous classes/Implement Runnable/src/Main.java
@@ -0,0 +1,16 @@
+
+class Create {
+
+    public static Runnable createRunnable(String text, int repeats) {
+        return new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < repeats; i++) {
+                    System.out.println(text);
+                }
+            }
+        };
+    }
+}
+
